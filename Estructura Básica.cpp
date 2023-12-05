45a63f82bf4996fcead9d626f10721213c2fc772
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:07:24 +0100

    Initial commit

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -30,7 +30,7 @@ public:
         }
     }
 
-    // Puedes agregar más funciones según sea necesario para manipular la tab
+    // Puedes agregar más funciones según sea necesario para manipular l
 };
 
 // Función principal para aplicaciones de Windows (WinMain)
