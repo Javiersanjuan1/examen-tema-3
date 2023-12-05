b9d1cc454ff1e40a6b9f8d29133597acb4d957c1
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:07:46 +0100

    Initial commit

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -30,7 +30,7 @@ public:
         }
     }
 
-    // Puedes agregar más funciones según sea necesario para mani
+    // Puedes agregar más funciones según s
 };
 
 // Función principal para aplicaciones de Windows (WinMain)
