078ec97c82936d4ecd64115fcfe8cbd678d9735b
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:11:33 +0100

    Initial commit

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -33,7 +33,7 @@ public:
 
 };
 
-// Función
+// F
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     // Crea una instancia de la clase Environment
     Environment myEnvironment;
