3ba829a3903ca62506f5a63a2d1694d5c2574efb
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:47:14 +0100

    Initial commit

--- Expandiendo Funcionalidades.cpp
+++ Expandiendo Funcionalidades.cpp
@@ -51,7 +51,7 @@ int main() {
     try {
         Environment myEnvironment;
 
-        // Agregar alg
+        // Agregar
         myEnvironment.addSymbol("x", Variant(10));
         myEnvironment.addSymbol("y", Variant("Hola, Mundo"));
 
