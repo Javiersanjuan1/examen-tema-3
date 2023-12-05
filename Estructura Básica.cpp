ae08ab80f20066b3371d744c355606c54b572abb
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:02:03 +0100

    Initial commit

---
+++ Estructura Básica.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <string>
+#include <Windows.h>
+
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+    Environment() {
+    }
+
+    void addSymbol(const std::string& symbol, int value) {
+        symbolTable[symbol] = value;
+    }
+
+    // Función para obtener el valor de un símbolo de la tabla de símbolos
+    int getSymbolValue(const std::string& symbol) const {
+        // Utiliza find() para buscar el símbolo en el std::map
+        auto it = symbolTable.find(symbol);
+
+        // Verifica si el símbolo está en la tabla y devuelve su valor
+        if (it != symbolTable.end()) {
+            return it->second;
+        } else {
+            // Puedes manejar el caso en que el símbolo no está en la tabla
+            // Puedes lanzar una excepción, devolver un valor predeterminado, etc.
+            // Por ahora, simplemente devolveremos 0 en caso de que no se encuentre.
+            return 0;
+        }
+    }
+
+    // Puedes agregar más funciones según sea necesario para manipular la tabla de símbolos
+};
+
+// Función principal para aplicaciones de Windows (WinMain)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Crea una instancia de la clase Environment
+    Environment myEnvironment;
+
+    // Tu código de la aplicación aquí
+
+    return 0;
+}
