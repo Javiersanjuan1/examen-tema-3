4324b0f366573d6ed182920250cd8bcf579027da
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:02:26 +0100

    Initial commit

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -14,7 +14,7 @@ public:
         symbolTable[symbol] = value;
     }
 
-    // Función para obtener el valor de un símbolo de la tabla de símbolo
+    // Función para obtener el valor de un símbolo de la tabla de símbol
     int getSymbolValue(const std::string& symbol) const {
         // Utiliza find() para buscar el símbolo en el std::map
         auto it = symbolTable.find(symbol);
