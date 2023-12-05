136cca4f54141c6f26f8b52b5672d5c9083d1f2b
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-05 11:40:37 +0100

    Initial commit

--- Expandiendo Funcionalidades.cpp
+++ Expandiendo Funcionalidades.cpp
@@ -20,7 +20,7 @@ public:
     Environment() {}
 
     void addSymbol(const std::string& symbol, const Variant& value) {
-        // Utiliza emplace para construir direc
+        // Utiliza emplace para construir di
         symbolTable.emplace(symbol, value);
     }
 
